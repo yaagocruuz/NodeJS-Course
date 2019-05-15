a540626c3f8d89cc2678b30c95d645ba5f6c1850
Yago Cruz <yaagocruuz@users.noreply.github.com>
2019-05-15 15:32:15 -0300

    added notes.js

---
+++ First Project/app.js
@@ -0,0 +1,8 @@
+const add = require('./utils.js')
+const getNotes = require('./notes')
+
+//const sum = add(4, -2)
+//console.log(sum)
+
+var notes = getNotes()
+console.log(notes)
---
+++ First Project/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js')
+
+const name = 'Yago'
+
+const add = function (a, b) {
+    return a + b
+}
+
+module.exports = add
