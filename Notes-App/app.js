7e3a38005f1e8cbf7cc10c701ebbe1ba3cf592f9
Yago Cruz <yaagocruuz@users.noreply.github.com>
2019-05-16 14:55:21 -0300

    end of section 3

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,11 +1,27 @@
-const validator = require('validator'); //npm package
+const validator = require('validator') //npm package
+const c = require('chalk')
+const chalk = c.default
 
-const getNotes = require('./notes.js');
+const getNotes = require('./notes.js')
 
-const msg = getNotes();
-console.log(msg);
+const msg = getNotes()
+console.log(msg)
 
-//console.log(validator.isEmail('example@example.com'));
-//console.log(validator.isEmail('example.com'));
+//console.log(validator.isEmail('example@example.com'))
+//console.log(validator.isEmail('example.com'))
+//console.log(validator.isURL('https://mead.io'))
 
-console.log(validator.isURL('htpps://www.google.com'));
+const log = console.log
+
+//warning and error
+const warningMsg = chalk.black.bgYellowBright
+const errorMsg = chalk.bgRedBright.black
+
+//my own theme
+//const myTheme = chalk.yellowBright.dim.bold.bgMagenta
+//log(myTheme('Testing my own theme'))
+//log(chalk.green('Success!'))
+
+//log error msg
+log(warningMsg('This is a warning!'));
+log(errorMsg('This is a error!'));
