966baa36d94f1c18a6c5ea97b80a62ef97e8ae49
Yago Cruz <yaagocruuz@users.noreply.github.com>
2019-05-20 00:19:59 -0300

    add .gitignore

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,27 +1,10 @@
-const validator = require('validator') //npm package
-const c = require('chalk')
+//NPM PACKAGES
+const validator = require('validator') //Validates emails, url's, etc
+const yargs = require('yargs') //Arguments on terminal
+const c = require('chalk') //Color for messages on terminal. 
 const chalk = c.default
-
-const getNotes = require('./notes.js')
-
-const msg = getNotes()
-console.log(msg)
-
-//console.log(validator.isEmail('example@example.com'))
-//console.log(validator.isEmail('example.com'))
-//console.log(validator.isURL('https://mead.io'))
 
 const log = console.log
 
-//warning and error
-const warningMsg = chalk.black.bgYellowBright
-const errorMsg = chalk.bgRedBright.black
-
-//my own theme
-//const myTheme = chalk.yellowBright.dim.bold.bgMagenta
-//log(myTheme('Testing my own theme'))
-//log(chalk.green('Success!'))
-
-//log error msg
-log(warningMsg('This is a warning!'));
-log(errorMsg('This is a error!'));
+log(process.argv)
+log(yargs.argv)
